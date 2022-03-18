9f0213ddcdb09802a54f585be7dd724fe353f7a7
Mohammed Abdulaziz <mohammed.knite25@gmail.com>
2022-03-18 23:04:50 +0300

    improve animation

--- app/components/skills.js
+++ app/components/skills.js
@@ -7,7 +7,7 @@ export default function Skills({ projectsRef }) {
                 <h1 data-aos="zoom-out-up" className="w-[240px] h-[70px] bg-blue-600 text-4xl text-text_primary font-bold p-4 mx-auto rounded">🔨 المهارات</h1>
             </div>
             <div className="cards-container rtl">
-                <div data-aos="flip-left" className="skill-card">
+                <div data-aos="fade-left" className="skill-card">
                     <div className="sm:w-[60%] md:w-[70%]">
                         <Image src="/svgs/web-development.svg" className="mx-auto" width="500" height="500" responsive="true" />
                     </div>
@@ -20,7 +20,7 @@ export default function Skills({ projectsRef }) {
                     </div>
                 </div>
 
-                <div data-aos="flip-left" className="skill-card">
+                <div data-aos="fade-right" className="skill-card">
                     <div className="sm:w-[58%] md:w-[70%]">
                         <Image src="/svgs/ui-ux.svg" width="300" height="300" responsive="true" />
                     </div>
@@ -32,10 +32,10 @@ export default function Skills({ projectsRef }) {
                 </div>
             </div>
             <div className="mx-auto text-center mt-3">
-                <h1 className="w-[240px] h-[70px] bg-blue-600 text-4xl text-text_primary font-bold p-4 mx-auto rounded">🧰 التقنيات</h1>
+                <h1 data-aos="zoom-out-up" className="w-[240px] h-[70px] bg-blue-600 text-4xl text-text_primary font-bold p-4 mx-auto rounded">🧰 التقنيات</h1>
             </div>
             <div className="cards-container">
-                <div data-aos="flip-left" className="skill-card">
+                <div data-aos="fade-right" className="skill-card">
                     <div className="sm:w-[58%] md:w-[70%]">
                         <Image src="/svgs/front-end.svg" width="300" height="300" responsive="true" />
                     </div>
@@ -50,7 +50,7 @@ export default function Skills({ projectsRef }) {
                         </ul>
                     </div>
                 </div>
-                <div data-aos="flip-left" className="skill-card">
+                <div data-aos="fade-left" className="skill-card">
                     <div className="sm:w-[60%] md:w-[70%]">
                         <Image src="/svgs/back-end.svg" className="mx-auto" width="500" height="500" responsive="true" />
                     </div>
